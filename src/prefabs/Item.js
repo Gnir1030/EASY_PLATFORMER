d9876f476d2c0602b9adf7be729d4e4e6d0983b7
kulkarnisheel@gmail.com <kulkarnisheel@gmail.com>
2022-05-15 19:19:07 -0700

    added Item prefab

--- src/prefabs/Item.js
+++ src/prefabs/Item.js
@@ -1,11 +1,11 @@
-class Portal extends Phaser.Physics.Arcade.Sprite {
-    constructor(scene, x, y, texture, frame, dest) {
+class Item extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene, x, y, texture, frame, chord) {
         //console.log(x + ", " + y);
         super(scene, x, y, texture, frame);
         scene.add.existing(this);
         scene.physics.add.existing(this);
-        this.destination = dest;
         this.setScale(1);
+        this.name = chord;
         this.body.allowGravity = false;
     }
 }
