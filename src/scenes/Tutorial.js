e32d107dc7020cd640ee5f1b6e7b1e0eaad2e859
louis01056 <louis01056@gmail.com>
2022-05-18 14:33:23 -0700

    UI scorll

--- src/scenes/Tutorial.js
+++ src/scenes/Tutorial.js
@@ -40,7 +40,7 @@ class Tutorial extends Phaser.Scene {
         keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
         this.add.text(20, 20, "Level 1").setScrollFactor(0);
         this.healthText = this.add.text(700, 20, "Health: " + 3).setScrollFactor(0);
-        this.add.text(84, 84, "Pick up the musical chord while avoiding the spikes").setScrollFactor(0);
+        this.add.text(84, 84, "Pick up the musical chord while avoiding the spikes").setScrollFactor(0); //UI scroll
 
 
         // map
