e52c53354766a150e19bd33dd2f3566e44bfb2ba
louis01056 <louis01056@gmail.com>
2022-05-18 14:33:03 -0700

    UI scroll

--- src/scenes/Tutorial.js
+++ src/scenes/Tutorial.js
@@ -38,9 +38,9 @@ class Tutorial extends Phaser.Scene {
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
         keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
         keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
-        this.add.text(20, 20, "Level 1");
-        this.healthText = this.add.text(700, 20, "Health: " + 3);
-        this.add.text(84, 84, "Pick up the musical chord while avoiding the spikes");
+        this.add.text(20, 20, "Level 1").setScrollFactor(0);
+        this.healthText = this.add.text(700, 20, "Health: " + 3).setScrollFactor(0);
+        this.add.text(84, 84, "Pick up the musical chord while avoiding the spikes").setScrollFactor(0);
 
 
         // map
@@ -122,6 +122,12 @@ class Tutorial extends Phaser.Scene {
             }
         }
     }
+
+    /*
+    render(){
+        this.debug.cameraInfo(this.cam, 32, 32);
+    }
+    */
 
     looseHealth() {
         this.player.health -= 1;
