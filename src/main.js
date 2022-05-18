c48e5da518ae7249ae54ed8871a6af8f964ad7fa
kulkarnisheel@gmail.com <kulkarnisheel@gmail.com>
2022-05-18 14:33:04 -0700

    added World 1 scene to be playable

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ let config = {
             debug: false
         }
     },
-    scene: [Menu, Tutorial, Hub]
+    scene: [Menu, Tutorial, Hub, World1]
     //scene: [Hub, World1]
 }
 let game = new Phaser.Game(config);
