038005271bb9cafef1c3763ce26509194c2658ca
louis01056 <louis01056@gmail.com>
2022-06-03 02:17:42 -0700

    menu update

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -35,8 +35,8 @@ class Menu extends Phaser.Scene {
         }
 
     // show menu text
-    this.add.text(config.width/2, config.height/2 - borderUISize - borderPadding, 'Finding Sound', menuConfig).setOrigin(0.5);
-    this.add.text(config.width/2, config.height/2, 'Use ←→ arrows to move, up arrow to Jump, & (Space) to shoot', menuConfig).setOrigin(0.5);
+    this.add.text(config.width/2, config.height/2 - borderUISize - borderPadding, 'Finding Color', menuConfig).setOrigin(0.5);
+    //this.add.text(config.width/2, config.height/2, 'Use ←→ arrows to move, up arrow to Jump, \n& (Space) to shoot', menuConfig).setOrigin(0.5);
     menuConfig.backgroundColor = '#00FF00';
     menuConfig.color = '#000';
     this.add.text(config.width/2, config.height/2 + borderUISize + borderPadding, 'Press ← to Play', menuConfig).setOrigin(0.5);
