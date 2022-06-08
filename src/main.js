3c37a2731600d6d71e893be928cc17b4b4b1aaf5
kulkarnisheel@gmail.com <kulkarnisheel@gmail.com>
2022-06-07 18:52:12 -0700

    mechanics cohesion update

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 //Zachary Hollaway, Sheel Kulkarni, Louis Lim
+//Title: M.W.A.
 //Published: 06/07/2022
 
 /*
@@ -6,7 +7,12 @@ Mechanics Cohesion
 The mechanics cohesion of our game is interaction with objects. In Metroidvania genre, players interact with three objects: enemies, items, and platforms.
 Among them, we focused on the interaction between enemies and items. In combat between the player and the enemy, updates to the in-game hitbox are implemented. 
 When the player is shot or hit by an enemy, he or she bounces off and cannot move for a while, but during this time, don't get damaged by the enemy. This is because the player can lose a lot of HP in a moment facing many enemies.
-The player can change the type of weapon freely by the situation, and there's an interaction that tells players how to use when getting close to it.
+The player can change the type of weapon freely by the situation, and there's an interaction that tells players how to use when getting close to it. The mechanic of swapping ammunition was difficult process and took some time to implement.
+We also created an enemy AI take can find the distance between the enemy and the player. The enemy can change direction in order to follow the player. It can also move up and down if the player is above or below it.
+We also implemented an internal timer that will allow the enemy to fire at the player once detected. This was a very complicated process and took many hours to complete.
+We are also really proud of our world transition mechanics. We believed that loading one giant world, would be quite taxing. So we decided to split it up into 3 seperate worlds and have another world that acts as a hub area.
+This hub world can go to multiple worlds in order. Meaning that once one world is completed, only then can the player move on to the next.
+There is also a feature that allows the player to restart the game for multiple playthroughs, once he or she is finished.
 
 Art Cohesion
 
