2258f9049fe98d0e03daad9d4c53a12349b7b152
louis01056 <louis01056@gmail.com>
2022-06-07 17:56:29 -0700

    cohesion justification

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 //Zachary Hollaway, Sheel Kulkarni, Louis Lim
-//Published: 05/15/2022
+//Published: 06/07/2022
+
+/*
+Mechanics Cohesion
+The mechanics cohesion of our game is interaction with objects. In Metroidvania genre, players interact with three objects: enemies, items, and platforms.
+Among them, we focused on the interaction between enemies and items. In combat between the player and the enemy, updates to the in-game hitbox are implemented. 
+When the player is shot or hit by an enemy, he or she bounces off and cannot move for a while, but during this time, don't get damaged by the enemy. This is because the player can lose a lot of HP in a moment facing many enemies.
+The player can change the type of weapon freely by the situation, and there's an interaction that tells players how to use when getting close to it.
+
+Art Cohesion
+
+*/
+
 let config = {
     type: Phaser.CANVAS,
     width: 800,
